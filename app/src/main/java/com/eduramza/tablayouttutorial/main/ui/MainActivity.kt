4f1c68877e7f3940a2b651519dbce02168c6a859
eduramza <eduardo.rmb@outlook.com>
2020-08-09 17:18:52 -0300

    init repository

---
+++ app/src/main/java/com/eduramza/tablayouttutorial/main/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+    package com.eduramza.tablayouttutorial.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eduramza.tablayouttutorial.R
+
+    class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
